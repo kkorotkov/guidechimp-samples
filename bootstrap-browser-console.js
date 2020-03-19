a5ed1f414f299dee0133cf4f8cb5a39a95bcb65e
Konstantin Korotkov <konstantin.korotkov@labs64.de>
2020-03-19 10:20:16 +0100

    Update bootstrap-browser-console.js

--- bootstrap-browser-console.js
+++ bootstrap-browser-console.js
@@ -1,26 +1,29 @@
-// Load GuideChimp scripts and styles
-function loadScript(url, onLoad, location) {
-    var script = document.createElement('script');
-    script.src = url;
-    if (onLoad) {
-        script.onload = onLoad;
-    }
-    location = location || document.head;
-    location.appendChild(script);
-
-    return script;
-}
-
 function loadStyles(href, location) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
     link.href = href;
-
     location = location || document.head;
     location.appendChild(link);
     return link;
 }
 
-loadScript('https://cdn.jsdelivr.net/npm/guidechimp@1/dist/guidechimp.min.js');
+function loadScript(url, onLoad, location) {
+    return new Promise((s, f) => {
+        var script = document.createElement('script');
+        script.src = url;
+        script.onload = function () {
+            if (onLoad) {
+                onLoad.apply(this);
+            }
+            s();
+        };
+        location = location || document.head;
+        location.appendChild(script);
+    });
+}
+
+// load styles
 loadStyles('https://cdn.jsdelivr.net/npm/guidechimp@1/dist/guidechimp.min.css');
+// load GuideChimp script and run guide
+loadScript('https://cdn.jsdelivr.net/npm/guidechimp@1/dist/guidechimp.min.js');
